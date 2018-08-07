2afa7987efc1e95f762c7e8251173be7c9fa2c2e
fantypants <8563615@gmail.com>
2018-08-07 09:59:40 -0700

    home page list is almost templated

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,10 +1,17 @@
 defmodule Cryptscrape.PageController do
   use Cryptscrape.Web, :controller
   alias Cryptscrape.Article
+  alias Cryptscrape.Domain
 
   def index(conn, _params) do
+    direct_query = from d in Domain, where: d.target == ^"Direct"
+    potential_query = from d in Domain, where: d.target == ^"Potential"
+    watch_query = from d in Domain, where: d.target == ^"Watch"
+    direct = Repo.all(direct_query) |> Repo.preload(:votes) |> Enum.uniq_by(fn(a) -> a.name end) |> Enum.take(5) |> IO.inspect
+    potential = Repo.all(potential_query) |> Repo.preload(:votes) |> Enum.uniq_by(fn(a) -> a.name end) |> Enum.take(5) |> IO.inspect
+    watch = Repo.all(watch_query) |> Repo.preload(:votes) |> Enum.uniq_by(fn(a) -> a.name end) |> Enum.take(5) |> IO.inspect
     #Article.get_information(:medium, "tronpay")
-    render conn, "index.html"
+    render conn, "index.html", direct: direct, potential: potential, watch: watch
   end
 
   def welcome(conn, _params) do
