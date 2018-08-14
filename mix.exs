9e6385b4c6d17f7b797c3b84e9ce6a3a4401622a
fantypants <8563615@gmail.com>
2018-08-13 18:46:14 -0700

    Email Adapter working

--- mix.exs
+++ mix.exs
@@ -52,6 +52,7 @@ defmodule Cryptscrape.Mixfile do
       {:stripity_stripe, "~> 2.0"},
       {:csvlixir, "~> 2.0.3"},
       {:csv, "~> 1.4.0"},
+      {:bamboo_smtp, "~> 1.5.0"},
       {:edeliver, "~> 1.4.2"},
       {:distillery, "~> 1.4"}]
   end
